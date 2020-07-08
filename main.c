631a23bd4df8b92d4ce7925fba0b6c233dfe2c55
浅倉麗子 <asakurareiko@protonmail.ch>
2020-07-08 17:39:17 -0400

    Add message after setting colour space

--- main.c
+++ main.c
@@ -16,7 +16,14 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 #include <psp2/avconfig.h>
+#include <psp2/display.h>
+#include <psp2/kernel/clib.h>
+#include <psp2/kernel/sysmem.h>
+#include <psp2/kernel/threadmgr.h>
 #include <psp2/registrymgr.h>
+#include "fnblit.h"
+
+extern char _binary_font_sfn_start[];
 
 int _newlib_heap_size_user = 4 * 1024;
 
@@ -26,5 +33,33 @@ int main(int argc, char **argv) { (void)argc; (void)argv;
 	sceRegMgrGetKeyInt("/CONFIG/DISPLAY/", "color_space_mode", &v);
 	sceAVConfigSetDisplayColorSpaceMode(!v);
 	sceRegMgrSetKeyInt("/CONFIG/DISPLAY/", "color_space_mode", !v);
+
+	SceUID mem_id = sceKernelAllocMemBlock(
+		"FramebufferMem",
+		SCE_KERNEL_MEMBLOCK_TYPE_USER_RW_UNCACHE,
+		SCE_KERNEL_2MiB,
+		NULL);
+	if (mem_id < 0) { goto done; }
+	void *fb_base;
+	sceKernelGetMemBlockBase(mem_id, &fb_base);
+	sceClibMemset(fb_base, 0x00, SCE_KERNEL_2MiB);
+
+	fnblit_set_font(_binary_font_sfn_start);
+	fnblit_set_fg(0xFFFFFFFF);
+	fnblit_set_bg(0x00000000);
+	fnblit_set_fb(fb_base, 960, 960, 544);
+	fnblit_printf(10, 10, "LCD colour space set to %s mode", !v ? "OLED emulation" : "original");
+	SceDisplayFrameBuf fb = {
+		sizeof(fb),
+		fb_base,
+		960,
+		SCE_DISPLAY_PIXELFORMAT_A8B8G8R8,
+		960,
+		544,
+	};
+	sceDisplaySetFrameBuf(&fb, SCE_DISPLAY_SETBUF_NEXTFRAME);
+	sceKernelDelayThread(1 * 1000 * 1000);
+
+done:
 	return 0;
 }
